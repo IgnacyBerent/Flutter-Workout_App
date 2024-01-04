cce596d0e61d7ddc588d0eff9566e16e8aa6987f
IgnacyBerent <2gb02ignac@gmail.com>
2024-01-04 04:15:50 +0100

    Adding exercise and training model

---
+++ lib/models/training.dart
@@ -0,0 +1,23 @@
+import 'package:workout_app/models/exercise.dart';
+
+enum Split {
+  I,
+  II,
+  III,
+  IV,
+  V,
+  VI,
+  VII,
+}
+
+class Training {
+  Training({
+    required this.date,
+    required this.split,
+    required this.exercises,
+  });
+
+  final String date;
+  final Split split;
+  final List<Exercise> exercises;
+}
