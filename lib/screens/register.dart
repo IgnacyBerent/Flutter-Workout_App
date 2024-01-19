5baff79b370b2e73cbf8d66a3e3832b57ad486ef
IgnacyBerent <2gb02ignac@gmail.com>
2024-01-19 13:51:31 +0100

    Password form improvment

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -23,6 +23,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
   var _enteredHeight = '';
   String? errorMessage = '';
 
+  bool _obscureText = true;
+
   Future<void> _signUp() async {
     FocusScope.of(context).unfocus(); // close keyboard
 
@@ -162,26 +164,38 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       ),
                     ],
                   ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      // Choose the icon based on the visibility state
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      // Change the state of the password visibility on button press
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
                 ),
-                obscureText: true,
+                obscureText: _obscureText,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter a password';
+                    return 'Enter a password';
                   }
                   if (value.length < 6) {
-                    return 'Password must be at least 6 characters long';
+                    return 'Minimum 6 characters';
                   }
                   if (!value.contains(RegExp(r'[A-Z]'))) {
-                    return 'Password must contain at least one uppercase letter';
+                    return 'Needs 1 uppercase';
                   }
                   if (!value.contains(RegExp(r'[a-z]'))) {
-                    return 'Password must contain at least one lowercase letter';
+                    return 'Needs 1 lowercase';
                   }
                   if (!value.contains(RegExp(r'[0-9]'))) {
-                    return 'Password must contain at least one digit';
+                    return 'Needs 1 digit';
                   }
                   if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
-                    return 'Password must contain at least one special character';
+                    return 'Needs 1 special char';
                   }
                   return null;
                 },
@@ -208,7 +222,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ],
                   ),
                 ),
-                obscureText: true,
+                obscureText: _obscureText,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter a password confirmation';
